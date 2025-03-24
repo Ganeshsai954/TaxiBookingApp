33dcbdbec8100e5b4f1e24c4355aae5d93259f85
Ganeshsai954 <Chinnuravipati4@gmail.com>
2025-03-24 23:59:34 +0530

    Developed Ride History Screen to view the list of booked rides and their details

---
+++ app/src/main/java/s9648482ravipatiganeshsai/taxi/bookingapp/PassengerProfileActivity.kt
@@ -0,0 +1,13 @@
+package s9648482ravipatiganeshsai.taxi.bookingapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PassengerProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
